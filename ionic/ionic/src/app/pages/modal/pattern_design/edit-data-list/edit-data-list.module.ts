3fcdf4c9ea85252d2a365e6d134762c9d26781c3
chanikan07 <60160335@go.buu.ac.th>
2020-03-11 02:59:29 +0700

    หน้าจอ modal การแก้ไขนะ

---
+++ ionic/ionic/src/app/pages/modal/pattern_design/edit-data-list/edit-data-list.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { EditDataListPage } from './edit-data-list.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EditDataListPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [EditDataListPage]
+})
+export class EditDataListPageModule {}
