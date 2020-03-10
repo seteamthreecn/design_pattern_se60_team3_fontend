f148162d132dd252473daf0a32a562270bc2d908
flukekazo55 <flukekazo55@gmail.com>
2020-03-11 02:55:22 +0700

    ทำ service ใหม่

--- ionic/ionic/src/app/service/ret-detail-list.service.ts
+++ ionic/ionic/src/app/service/ret-detail-list.service.ts
@@ -17,16 +17,18 @@ export class RetDetailListService {
 	 * @author Chutipong
 	 * @Create Date 2563-03-09
 	 */
-  insert(dtl_amount: any, dtl_date: any, dtl_type: any, dtl_dts_id: any) {
+  insert(dtl_amount: any, dtl_date: any, dtl_type: any, dtl_dts_id: any, dtl_description: any) {
     let data = {
       'dtl_amount': dtl_amount,
       'dtl_date': dtl_date,
       'dtl_type': dtl_type,
-      'dtl_dts_id': dtl_dts_id
+      'dtl_dts_id': dtl_dts_id,
+      'dtl_description' : dtl_description
     }
+    console.log(data)
 
     return this.http
-      .post("http://localhost:83/insert_detail_sub_type", data)
+      .post("http://localhost:83/insert_ret_detail_list", data)
       .map(res => res.json());
 
   }
@@ -39,17 +41,18 @@ export class RetDetailListService {
 	 * @author Chutipong
 	 * @Create Date 2563-03-09
 	 */
-  update(dtl_id: any, dtl_amount: any, dtl_date: any, dtl_type: any, dtl_dts_id: any) {
+  update(dtl_id: any, dtl_amount: any, dtl_date: any, dtl_type: any, dtl_dts_id: any, dtl_description: any) {
     let data = {
       'dtl_id': dtl_id,
       'dtl_amount': dtl_amount,
       'dtl_date': dtl_date,
       'dtl_type': dtl_type,
-      'dtl_dts_id': dtl_dts_id
+      'dtl_dts_id': dtl_dts_id,
+      'dtl_description' : dtl_description
     }
 
     return this.http
-      .post("http://localhost:83/insert_detail_sub_type", data)
+      .post("http://localhost:83/update_ret_detail_list", data)
       .map(res => res.json());
 
   }
@@ -68,7 +71,7 @@ export class RetDetailListService {
     }
 
     return this.http
-      .post("http://localhost:83/insert_detail_sub_type", data)
+      .post("http://localhost:83/delete_ret_detail_list", data)
       .map(res => res.json());
 
   }
@@ -87,7 +90,22 @@ export class RetDetailListService {
     }
 
     return this.http
-      .post("http://localhost:83/insert_detail_sub_type", data)
+      .post("http://localhost:83/ret_detail_list_by_dtl_id", data)
+      .map(res => res.json());
+
+  }
+
+  /*
+	 * get_last
+	 * ..
+	 * @input -
+	 * @output -
+	 * @author Chutipong
+	 * @Create Date 2563-03-09
+	 */
+  get_last() {
+    return this.http
+      .post("http://localhost:83/get_last_ret_detail_list", "")
       .map(res => res.json());
 
   }
